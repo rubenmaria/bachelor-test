a8e35641039029b0c0ff13cd0cca2b40fea5015d
Lyrexes <steamkonto60@gmail.com>
2024-01-24 09:34:41 +0100

    first graph

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import os
+import numpy as np
+from numpy._typing import NDArray
+from sklearn.manifold import TSNE
 import clang.cindex
 from clang.cindex import Cursor
 from clang.cindex import SourceRange
 from sentence_transformers import SentenceTransformer
-
+from matplotlib import pyplot as plt
 GLIBC_PATH = "glibc"
 
 def main():
     src_files = get_src_files(GLIBC_PATH)
     functions = get_n_functions(src_files, 100)
-    print(function_to_embedding(functions[0]))
+    embeddings = [function_to_embedding(functions[i]) for i in range(10)]
+    embeddings = make_same_dimensions(embeddings)
+    low_dimension_embeddings = embedding_to_low_dimension(np.array(embeddings))
+    print("after: ", low_dimension_embeddings.shape)
+    print(low_dimension_embeddings)
+    draw_2d_embeddings(low_dimension_embeddings, [x.split("\n")[0] for x in functions[:10]])
+
+def make_same_dimensions(embeddings: list[NDArray]) -> list[NDArray]:
+    max_dimension = max([e.shape[0] for e in embeddings])
+    result = list()
+    for e in embeddings:
+        delta_dimension = max_dimension - e.shape[0]
+        if delta_dimension > 0:
+            result.append(np.concatenate((e,np.zeros(delta_dimension))))
+        else:
+            result.append(e)
+    return result
+
+    
+def draw_2d_embeddings(embedding: NDArray, names: list[str]):
+    plt.figure(figsize=(6, 5))
+    colors = 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'grey', 'orange', 'purple'
+    for i, c, n in zip(range(embedding.shape[0]), colors, names):
+        plt.scatter(embedding[i, 0], embedding[i, 1], c=c, label=n)
+    plt.legend()
+    plt.show()
     
 
-def function_to_embedding(function: str):
+def function_to_embedding(function: str) -> NDArray:
     model = SentenceTransformer('all-MiniLM-L6-v2')
     lines = function.split("\n")
-    return model.encode(lines, convert_to_numpy=True)
+    embedding_raw = np.array(model.encode(lines, convert_to_numpy=True))
+    embedding = np.array(list())
+    for emb in embedding_raw:
+        embedding = np.concatenate((embedding, emb))
+    return embedding
+
+def embedding_to_low_dimension(embedding: NDArray) -> NDArray:
+    return TSNE(perplexity=5).fit_transform(embedding)
+
 
 
 def get_src_files(glibc_path: str) -> list[str]:
