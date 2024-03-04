8be865e02a07aca7366bdacad9dbf051cd0c7459
rubenmaria <steamkonto60@gmail.com>
2024-03-04 12:54:27 +0100

    changed prompt

--- llama-prompt.py
+++ llama-prompt.py
@@ -3,35 +3,42 @@ import time
 from typing import Optional
 from llama import Llama
 
-CHECKPOINT_DIR: str = "/mnt/ambrym1/llama-model/llama/llama-2-7b-chat/"
+CHECKPOINT_DIR: str = "/mnt/ambrym1/llama-model/llama/llama-2-13b-chat/"
 TOKENIZER_PATH: str = "/mnt/ambrym1/llama-model/llama/tokenizer.model"
-MAX_SEQUENCE_LENGTH: int = 2048
+MAX_SEQUENCE_LENGTH: int = 8192 * 2
 MAX_BATCH_SIZE: int  = 6
 TEMPERATURE: float  = 0.6
 TOP_P: float = 0.9
 MAX_GEN_LENGTH: Optional[int] = None
 
 def main():
-    with open("data/function-definition-table.json") as file:
+    """
+    with open("data/function-defintions.json") as file:
         definitions = json.load(file)
-        llama_prompt = "Kannst du mir die Funktion erklären?\n"
+        llama_prompt = "Can you briefly summarize in one two sentence what the following function does? And can you give just the summary?\n"
         llama_generator = build_llama_generator()
         for definition_name in definitions.keys():
             print(definition_name + ": " +  prompt_llama(
                 llama_generator,
                 llama_prompt + definitions[definition_name]
             ))
+    """
+    dump_llama_function_explanation()
 
 def dump_llama_function_explanation() -> None:
-    with open("data/function-definition-table.json") as file:
+    with open("data/function-defintions.json") as file:
+        llama_generator = build_llama_generator()
         definitions = json.load(file)
         llama_out = {}
-        llama_prompt = "Kannst du mir die Funktion erklären?\n"
+        llama_prompt = "Can you briefly summarize in one two sentence what the following function does? And can you give just the summary?\n"
         for definition_name in definitions.keys():
-            llama_out[definition_name] = prompt_llama(
+            response = prompt_llama(
+                llama_generator,
                 llama_prompt + definitions[definition_name]
             )
-        with open("function-definition-explanation.json") as f:
+            print(response)
+            llama_out[definition_name] = response
+        with open("function-definition-explanation.json", w) as f:
             json.dump(llama_out, f)
 
 
@@ -46,7 +53,10 @@ def build_llama_generator():
 
 def prompt_llama(generator, msg: str) -> str:
 
-    dialogs = [[{"role": "user", "content": msg}]]
+    dialogs = [[
+        {"role": "system", "content": "Just give the summary nothing else!"},
+        {"role": "user", "content": msg}
+        ]]
     results = generator.chat_completion(
         dialogs,  # type: ignore
         max_gen_len=MAX_GEN_LENGTH,
