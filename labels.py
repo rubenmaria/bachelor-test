6657960bbdab16d5dab9a103b9a5bcdb93356da5
Lyrexes <steamkonto60@gmail.com>
2024-06-10 10:48:15 +0200

    now can continue generating labels after crash

--- labels.py
+++ labels.py
@@ -18,7 +18,11 @@ def generate_labels(
     function_data_path: str,
     model_name: str
     ) -> None:
-    functions = load_function_data(function_data_path)
+    functions = get_missing_functions(
+        function_data_path,
+        output_directory,
+        output_name
+    )
     for function in progress_bar(functions, prefix="Generating embeddings", decimals=6):
         if function.srccode is None:
             continue
@@ -26,10 +30,32 @@ def generate_labels(
         append_label(output_directory, output_name, label)
 
 
+def get_missing_functions(
+    functions_path: str,
+    output_directory: str,
+    output_name: str
+    ) -> list[FuncData]:
+    functions = load_function_data(functions_path)
+    labels = load_label_data(
+        get_output_file_path(output_directory, output_name)
+    )
+    processed_indecies = [l.index for l in labels]
+    return [f for f in functions if f.index not in processed_indecies]
+
+
 def load_function_data(path: str) -> list[FuncData]:
     with open(path, "rb") as f:
         return pickle.load(f)
 
+def load_label_data(path: str) -> list[LabelData]:
+    with open(path, "rb") as f:
+        label_data = []
+        while True:
+            try:
+                label_data.append(pickle.load(f))
+            except EOFError:
+                break
+    return label_data
 
 def get_label_from_function(func: FuncData, model_name: str) -> LabelData:
     summary = get_summary(func.srccode, model_name, PROMPT_PATH)
@@ -37,8 +63,12 @@ def get_label_from_function(func: FuncData, model_name: str) -> LabelData:
     return LabelData(func.func_id, embedding, summary)
 
 
+def get_output_file_path(directory: str, name: str) -> str:
+    return os.path.join(directory, name + ".pkl")
+
+
 def dump_labels(directory: str, name: str, labels: list[LabelData]) -> None:
-    file_path = os.path.join(directory, name + ".pkl")
+    file_path = get_output_file_path(directory, name)
     with open(file_path, "wb") as file:
         pickle.dump(labels, file)
 
